1bc0b868fff640b88356a78e1ae9004777bb3387
kai staud <kai-staud@web.de>
2023-08-27 11:37:36 +0200

    init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /hdd/liblpb
+# Build directory: /hdd/liblpb/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ lpb_shmem.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void write_wo_od(int data);
+void access_ro_od();
+void open_ro_od();
+void close_ro_od();
+void open_wo_od();
+void close_wo_od();
+
